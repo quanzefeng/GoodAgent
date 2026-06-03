a792378dbb349171dc8670969b350a2bc9ce2b64
quanzefeng <2993919594@qq.com>
2026-06-03 10:16:51 +0800

    chore: remove 16 dead files (~600KB), cleanup package.json and .gitignore

--- desktop/minimal_preload.js
+++
@@ -1,8 +0,0 @@
-process.stderr.write("[minimal_preload] RUNNING\n");
-console.error("[minimal_preload] RUNNING via console.error\n");
-try {
-  const e = require("electron");
-  console.error("[minimal_preload] electron loaded:", Object.keys(e).join(", "));
-} catch (x) {
-  console.error("[minimal_preload] require(electron) failed:", x.message);
-}
